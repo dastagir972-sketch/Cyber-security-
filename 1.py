5dae08a2b79c60a12dc8b9f6fd90d0ccca2f20a7
dastagir972-sketch <ddastagir972@gmail.com>
2025-08-19 12:34:42 +0530

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,12 @@
+temp=int(input("enter the temprature"))
+if(temp<0):
+	print("freezing wether")
+elif(temp<10):
+	print("very cold")
+elif(temp<20):
+	print("normal cold ")
+elif(temp<30):
+	print("cold whether")
+else:
+	print("itz too hot")
+	
